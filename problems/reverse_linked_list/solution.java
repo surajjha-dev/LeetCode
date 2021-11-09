87ed94067316470ca96600dad6b645342fe05f94
Suraj Jha <25772037+surajjha-dev@users.noreply.github.com>
2021-11-09 06:51:47 +0000

    Sync LeetCode submission - Reverse Linked List (java)

--- problems/reverse_linked_list/solution.java
+++ problems/reverse_linked_list/solution.java
@@ -7,22 +7,33 @@
  *     ListNode(int val) { this.val = val; }
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
+ 
+    given - 1->2->3->4->5-> Null
+    return - 5->4->3->2->1->NULL 
+ 
+    prev = null;
+    next = 2;
+    curr.next = prev;
+    prev = curr;
+    curr = next;
+ 
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-           
-          if (head == null || head.next == null) return head;
+        
+       if (head == null || head.next == null)
+               return head;
             
-          ListNode curr = head;
-          ListNode prev = null;
-            
-          while (curr != null) {
-              ListNode next = curr.next;
-              curr.next = prev;
-              prev = curr;
-              curr = next;
-          }  
-            
-          return prev;  
-    }
+       ListNode previous = null;
+       ListNode current = head;
+       
+       while (current != null) {
+           ListNode next = current.next;
+           current.next = previous;
+           previous = current;
+           current = next;    
+       }       
+               
+       return previous;        
+  }
 }
