3eb0a4d4a5f2bcd59742f5dab3f9085a4711af4f
Suraj Jha <25772037+surajjha-dev@users.noreply.github.com>
2022-06-09 05:43:40 +0000

    Sync LeetCode submission - Add Two Numbers II (java)

---
+++ problems/add_two_numbers_ii/solution.java
@@ -0,0 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        l1 = reverse(l1);
+        l2 = reverse(l2);
+            
+        int carry = 0;  // to forward the carry after adding the numbers
+            
+        ListNode p, dummy = new ListNode(0);
+        p = dummy;            
+        while ( l1 != null || l2 != null || carry != 0 ) {
+            
+           if ( l1 != null ) {
+                    carry += l1.val;
+                    l1 = l1.next;
+                }
+                    
+                if ( l2 != null ) {
+                    carry += l2.val;
+                    l2 = l2.next;
+                }
+                 
+                p.next = new ListNode(carry % 10);
+                carry = carry / 10;
+                p = p.next;       
+        }
+            
+        return reverse(dummy.next);    
+    }
+        
+        
+    public ListNode reverse(ListNode node) {
+            ListNode prev = null;
+            ListNode curr = node;
+            
+            while (curr != null) {
+                 ListNode next = curr.next;
+                 curr.next = prev;
+                 prev = curr;
+                 curr = next;    
+            }
+            
+            return prev;
+    }    
+}
